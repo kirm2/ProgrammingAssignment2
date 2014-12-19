f5ced0a2fc988dd549cbad3b51fc128559e87740
kirm2 <kirill.mikhailov@outlook.com>
2014-12-19 11:23:11 +0300

    Added comments
    Signed-off-by: kirm2 <kirill.mikhailov@outlook.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,21 @@
+## package of functions designed to avoid repeated inverse matrix calculation
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix - wraps matrix into object with ability to cache inverse matrix.
+##
+## Usage:
+##
+## makeCacheMatrix(x = matrix()) 
+##
+## x - is a matrix to be wrapped. If omitted the object returned has empty matrix
+##
+## return value - object with folowing methods
+##   get()          - returns wrapped matrix
+##   set(x)         - sets new matrix and resets cached inverse value
+##   getinverse()   - returns cached inverse value if available or NULL if not
+##   setinverse(x)  - sets new inverse value 
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -20,7 +34,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## cacheSolve - calculates and caches inverse matrix of an argument
+##
+## Usage:
+##
+## cacheSolve(x) where x is a wrapped matrix returned from makeCacheMatrix
+##
+## return value - inverse of the matrix wrapped in x. If x had cached value
+## of inverse matrix the cached value is returned. Return value is a raw matrix
+## not a result of makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
